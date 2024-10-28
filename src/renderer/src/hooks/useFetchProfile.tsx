6d9f815bc4fdb0a3fa3a1f1bf899c4682d64b431
codytseng <codytseng@outlook.com>
2024-10-28 10:24:03 +0800

    🐛

--- src/renderer/src/hooks/useFetchProfile.tsx
+++ src/renderer/src/hooks/useFetchProfile.tsx
@@ -1,7 +1,7 @@
 import { formatNpub } from '@renderer/lib/pubkey'
 import client from '@renderer/services/client.service'
 import { nip19 } from 'nostr-tools'
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 type TProfile = {
   username: string
@@ -27,39 +27,25 @@ const decodeUserId = (id: string): { pubkey?: string; npub?: `npub1${string}` } 
 }
 
 export function useFetchProfile(id?: string) {
-  const initialProfile: TProfile = useMemo(() => {
-    const profile: TProfile = {
-      username: id ? (id.length > 9 ? id.slice(0, 4) + '...' + id.slice(-4) : id) : 'username'
-    }
-    if (!id) return profile
-
-    const { pubkey, npub } = decodeUserId(id)
-    if (!pubkey || !npub) return profile
-
-    return {
-      ...profile,
-      username: formatNpub(npub),
-      pubkey,
-      npub
-    }
-  }, [id])
+  const initialProfile: TProfile = {
+    username: id ? (id.length > 9 ? id.slice(0, 4) + '...' + id.slice(-4) : id) : 'username'
+  }
   const [profile, setProfile] = useState<TProfile>(initialProfile)
 
   const fetchProfile = useCallback(async () => {
     try {
-      const { pubkey, npub } = initialProfile
-      if (!pubkey || !npub) {
-        return
-      }
+      if (!id) return
+
+      const { pubkey, npub } = decodeUserId(id)
+      if (!pubkey || !npub) return
 
       const profileEvent = await client.fetchEventWithCache({
         authors: [pubkey],
         kinds: [0]
       })
-      if (!profileEvent) {
-        setProfile({ ...initialProfile, pubkey, npub })
-        return
-      }
+      const username = npub ? formatNpub(npub) : initialProfile.username
+      setProfile({ pubkey, npub, username })
+      if (!profileEvent) return
 
       const profileObj = JSON.parse(profileEvent.content)
       setProfile({
@@ -69,18 +55,18 @@ export function useFetchProfile(id?: string) {
         banner: profileObj.banner,
         avatar: profileObj.picture,
         username:
-          profileObj.display_name.trim() || profileObj.name.trim() || initialProfile.username,
+          profileObj.display_name?.trim() || profileObj.name?.trim() || initialProfile.username,
         nip05: profileObj.nip05,
         about: profileObj.about
       })
-    } catch {
-      // ignore
+    } catch (err) {
+      console.error(err)
     }
-  }, [initialProfile])
+  }, [id])
 
   useEffect(() => {
     fetchProfile()
-  }, [fetchProfile])
+  }, [id])
 
   return profile
 }
