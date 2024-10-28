d37c9a196a073b6e7d97c4387152bb82e9bf5da2
codytseng <codytseng@outlook.com>
2024-10-28 09:33:58 +0800

    🐛

--- src/renderer/src/hooks/useFetchProfile.tsx
+++ src/renderer/src/hooks/useFetchProfile.tsx
@@ -1,7 +1,7 @@
 import { formatNpub } from '@renderer/lib/pubkey'
-import { nip19 } from 'nostr-tools'
-import { useEffect, useMemo, useState } from 'react'
 import client from '@renderer/services/client.service'
+import { nip19 } from 'nostr-tools'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 
 type TProfile = {
   username: string
@@ -13,6 +13,19 @@ type TProfile = {
   about?: string
 }
 
+const decodeUserId = (id: string): { pubkey?: string; npub?: `npub1${string}` } => {
+  if (/^npub1[a-z0-9]{58}$/.test(id)) {
+    const { data } = nip19.decode(id as `npub1${string}`)
+    return { pubkey: data, npub: id as `npub1${string}` }
+  } else if (id.startsWith('nprofile1')) {
+    const { data } = nip19.decode(id as `nprofile1${string}`)
+    return { pubkey: data.pubkey, npub: nip19.npubEncode(data.pubkey) }
+  } else if (/^[0-9a-f]{64}$/.test(id)) {
+    return { pubkey: id, npub: nip19.npubEncode(id) }
+  }
+  return {}
+}
+
 export function useFetchProfile(id?: string) {
   const initialProfile: TProfile = useMemo(() => {
     const profile: TProfile = {
@@ -20,20 +33,7 @@ export function useFetchProfile(id?: string) {
     }
     if (!id) return profile
 
-    let pubkey: string | undefined
-    let npub: `npub1${string}` | undefined
-    if (/^npub1[a-z0-9]{58}$/.test(id)) {
-      const { data } = nip19.decode(id as `npub1${string}`)
-      pubkey = data
-      npub = id as `npub1${string}`
-    } else if (id.startsWith('nprofile1')) {
-      const { data } = nip19.decode(id as `nprofile1${string}`)
-      pubkey = data.pubkey
-      npub = nip19.npubEncode(pubkey)
-    } else if (/^[0-9a-f]{64}$/.test(id)) {
-      pubkey = id
-      npub = nip19.npubEncode(id)
-    }
+    const { pubkey, npub } = decodeUserId(id)
     if (!pubkey || !npub) return profile
 
     return {
@@ -45,41 +45,41 @@ export function useFetchProfile(id?: string) {
   }, [id])
   const [profile, setProfile] = useState<TProfile>(initialProfile)
 
-  useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const { pubkey, npub } = initialProfile
-        if (!pubkey || !npub) {
-          return
-        }
-
-        const profileEvent = await client.fetchEventWithCache({
-          authors: [pubkey],
-          kinds: [0]
-        })
-        if (!profileEvent) {
-          setProfile({ ...profile, pubkey, npub })
-          return
-        }
-
-        const profileObj = JSON.parse(profileEvent.content)
-        setProfile({
-          ...profile,
-          pubkey,
-          npub,
-          banner: profileObj.banner,
-          avatar: profileObj.picture,
-          username: profileObj.display_name || profileObj.name || profile.username,
-          nip05: profileObj.nip05,
-          about: profileObj.about
-        })
-      } catch {
-        // ignore
+  const fetchProfile = useCallback(async () => {
+    try {
+      const { pubkey, npub } = initialProfile
+      if (!pubkey || !npub) {
+        return
       }
-    }
 
+      const profileEvent = await client.fetchEventWithCache({
+        authors: [pubkey],
+        kinds: [0]
+      })
+      if (!profileEvent) {
+        setProfile({ ...initialProfile, pubkey, npub })
+        return
+      }
+
+      const profileObj = JSON.parse(profileEvent.content)
+      setProfile({
+        ...initialProfile,
+        pubkey,
+        npub,
+        banner: profileObj.banner,
+        avatar: profileObj.picture,
+        username: profileObj.display_name || profileObj.name || profile.username,
+        nip05: profileObj.nip05,
+        about: profileObj.about
+      })
+    } catch {
+      // ignore
+    }
+  }, [initialProfile])
+
+  useEffect(() => {
     fetchProfile()
-  }, [id])
+  }, [fetchProfile])
 
   return profile
 }
