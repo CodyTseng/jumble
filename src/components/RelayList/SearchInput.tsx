fdb90de652ae14d15283f1f39a6de6b5f2dbbb71
codytseng <codytseng@outlook.com>
2025-02-06 12:18:45 +0800

    🎨

--- src/components/RelayList/SearchInput.tsx
+++ src/components/RelayList/SearchInput.tsx
@@ -16,7 +16,7 @@ export default function SearchInput({ value, onChange, ...props }: ComponentProp
         'flex h-9 w-full items-center rounded-md border border-input bg-transparent px-2 py-1 text-base shadow-sm transition-colors md:text-sm [&:has(:focus-visible)]:ring-ring [&:has(:focus-visible)]:ring-1 [&:has(:focus-visible)]:outline-none'
       )}
     >
-      <SearchIcon className="size-4 shrink-0" />
+      <SearchIcon className="size-4 shrink-0 opacity-50" />
       <input
         {...props}
         value={value}
@@ -25,7 +25,7 @@ export default function SearchInput({ value, onChange, ...props }: ComponentProp
       />
       {displayClear && (
         <button type="button" onClick={() => onChange?.({ target: { value: '' } } as any)}>
-          <X className="size-4 shrink-0" />
+          <X className="size-4 shrink-0 opacity-50 hover:opacity-100" />
         </button>
       )}
     </div>
