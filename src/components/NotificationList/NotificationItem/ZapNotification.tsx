d6de7320a86f57f5b5f2bb6516ac965c34e08097
codytseng <codytseng98@gmail.com>
2025-02-16 15:08:30 +0800

    💨

--- src/components/NotificationList/NotificationItem/ZapNotification.tsx
+++ src/components/NotificationList/NotificationItem/ZapNotification.tsx
@@ -1,8 +1,9 @@
 import { useFetchEvent } from '@/hooks'
 import { formatAmount, getAmountFromInvoice } from '@/lib/lightning'
-import { toNote } from '@/lib/link'
+import { toNote, toProfile } from '@/lib/link'
 import { tagNameEquals } from '@/lib/tag'
 import { useSecondaryPage } from '@/PageManager'
+import { useNostr } from '@/providers/NostrProvider'
 import { Zap } from 'lucide-react'
 import { Event } from 'nostr-tools'
 import { useMemo } from 'react'
@@ -12,12 +13,24 @@ import { ContentPreview } from './ContentPreview'
 
 export function ZapNotification({ notification }: { notification: Event }) {
   const { push } = useSecondaryPage()
-  const { senderPubkey, eventId, amount } = useMemo(() => {
-    const senderPubkey = notification.tags.find(tagNameEquals('P'))?.[1]
-    const eventId = notification.tags.find(tagNameEquals('e'))?.[1]
-    const invoice = notification.tags.find(tagNameEquals('bolt11'))?.[1]
-    const amount = invoice ? getAmountFromInvoice(invoice) : 0
-    return { senderPubkey, eventId, amount }
+  const { pubkey } = useNostr()
+  const { senderPubkey, eventId, amount, comment } = useMemo(() => {
+    const result: { senderPubkey?: string; eventId?: string; amount?: number; comment?: string } =
+      {}
+    try {
+      result.senderPubkey = notification.tags.find(tagNameEquals('P'))?.[1]
+      result.eventId = notification.tags.find(tagNameEquals('e'))?.[1]
+      const invoice = notification.tags.find(tagNameEquals('bolt11'))?.[1]
+      result.amount = invoice ? getAmountFromInvoice(invoice) : 0
+      const description = notification.tags.find(tagNameEquals('description'))?.[1]
+      if (description) {
+        const zapRequest = JSON.parse(description)
+        result.comment = zapRequest.content
+      }
+    } catch {
+      // ignore
+    }
+    return result
   }, [notification])
   const { event } = useFetchEvent(eventId)
 
@@ -25,14 +38,17 @@ export function ZapNotification({ notification }: { notification: Event }) {
 
   return (
     <div
-      className="flex gap-2 items-center cursor-pointer py-2"
-      onClick={() => event && push(toNote(event))}
+      className="flex items-center justify-between cursor-pointer py-2"
+      onClick={() => (event ? push(toNote(event)) : pubkey ? push(toProfile(pubkey)) : null)}
     >
-      <UserAvatar userId={senderPubkey} size="small" />
-      <Zap size={24} className="text-yellow-400" />
-      <div className="font-semibold text-yellow-400">{formatAmount(amount)} stats</div>
-      <ContentPreview event={event} />
-      <div className="text-muted-foreground">
+      <div className="flex gap-2 items-center flex-1 w-0">
+        <UserAvatar userId={senderPubkey} size="small" />
+        <Zap size={24} className="text-yellow-400 shrink-0" />
+        <div className="font-semibold text-yellow-400 shrink-0">{formatAmount(amount)} stats</div>
+        {comment && <div className="text-yellow-400 truncate">{comment}</div>}
+        <ContentPreview event={event} />
+      </div>
+      <div className="text-muted-foreground shrink-0">
         <FormattedTimestamp timestamp={notification.created_at} short />
       </div>
     </div>
