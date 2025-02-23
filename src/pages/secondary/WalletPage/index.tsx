ad214c26f890e9636cbbabe19049ec641a19e292
codytseng <codytseng98@gmail.com>
2025-02-23 16:58:17 +0800

    🏗️

--- src/pages/secondary/WalletPage/index.tsx
+++ src/pages/secondary/WalletPage/index.tsx
@@ -1,55 +1,21 @@
-import { Input } from '@/components/ui/input'
 import SecondaryPageLayout from '@/layouts/SecondaryPageLayout'
-import { useZap } from '@/providers/ZapProvider'
 import { Button as BcButton } from '@getalby/bitcoin-connect-react'
-import { forwardRef, useState } from 'react'
+import { forwardRef } from 'react'
 import { useTranslation } from 'react-i18next'
+import DefaultZapAmountInput from './DefaultZapAmountInput'
+import DefaultZapCommentInput from './DefaultZapCommentInput'
 import LightningAddressInput from './LightningAddressInput'
 
 const WalletPage = forwardRef(({ index }: { index?: number }, ref) => {
   const { t } = useTranslation()
-  const { defaultZapSats, updateDefaultSats, defaultZapComment, updateDefaultComment } = useZap()
-  const [defaultZapAmountInput, setDefaultZapAmountInput] = useState(defaultZapSats)
-  const [defaultZapCommentInput, setDefaultZapCommentInput] = useState(defaultZapComment)
 
   return (
     <SecondaryPageLayout ref={ref} index={index} title={t('Wallet')}>
       <div className="px-4 pt-2 space-y-4">
         <BcButton />
         <LightningAddressInput />
-        <div className="flex justify-between items-center">
-          <div>{t('Default zap amount')}</div>
-          <Input
-            className="w-40"
-            value={defaultZapAmountInput}
-            onChange={(e) => {
-              setDefaultZapAmountInput((pre) => {
-                if (e.target.value === '') {
-                  return 0
-                }
-                let num = parseInt(e.target.value, 10)
-                if (isNaN(num) || num < 0) {
-                  num = pre
-                }
-                return num
-              })
-            }}
-            onBlur={() => {
-              updateDefaultSats(defaultZapAmountInput)
-            }}
-          />
-        </div>
-        <div className="flex justify-between items-center">
-          <div>{t('Default zap comment')}</div>
-          <Input
-            className="w-40"
-            value={defaultZapCommentInput}
-            onChange={(e) => setDefaultZapCommentInput(e.target.value)}
-            onBlur={() => {
-              updateDefaultComment(defaultZapCommentInput)
-            }}
-          />
-        </div>
+        <DefaultZapAmountInput />
+        <DefaultZapCommentInput />
       </div>
     </SecondaryPageLayout>
   )
