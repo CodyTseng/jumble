104d4bc30188afd38ece2f0091534f542c2d6a63
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2025-05-22 23:17:47 -0600

    refactor(i18n): update translations

--- src/i18n/locales/de.ts
+++ src/i18n/locales/de.ts
@@ -44,6 +44,7 @@ export default {
     Quote: 'Zitat',
     'Copy event ID': 'Ereignis-ID kopieren',
     'Copy user ID': 'Benutzer-ID kopieren',
+    'Copy share link': 'Kopieren Sie den Link',
     'View raw event': 'Rohdaten anzeigen',
     Like: 'Gefällt mir',
     'switch to light theme': 'Wechsel zum hellen Design',
