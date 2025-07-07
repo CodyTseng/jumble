35d32ff2b22ebeb8dfb45b5b47030b184aa3600c
codytseng <codytseng98@gmail.com>
2025-07-07 22:15:08 +0800

    💨

--- src/components/NotificationList/NotificationItem/ReactionNotification.tsx
+++ src/components/NotificationList/NotificationItem/ReactionNotification.tsx
@@ -1,5 +1,4 @@
 import Image from '@/components/Image'
-import { ExtendedKind } from '@/constants'
 import { useFetchEvent } from '@/hooks'
 import { toNote } from '@/lib/link'
 import { tagNameEquals } from '@/lib/tag'
@@ -7,7 +6,7 @@ import { cn } from '@/lib/utils'
 import { useSecondaryPage } from '@/PageManager'
 import { useNostr } from '@/providers/NostrProvider'
 import { Heart } from 'lucide-react'
-import { Event, kinds } from 'nostr-tools'
+import { Event } from 'nostr-tools'
 import { useMemo } from 'react'
 import ContentPreview from '../../ContentPreview'
 import { FormattedTimestamp } from '../../FormattedTimestamp'
@@ -54,13 +53,7 @@ export function ReactionNotification({
     return notification.content
   }, [notification])
 
-  if (
-    !event ||
-    !eventId ||
-    ![kinds.ShortTextNote, kinds.Highlights, ExtendedKind.PICTURE, ExtendedKind.COMMENT].includes(
-      event.kind
-    )
-  ) {
+  if (!event || !eventId) {
     return null
   }
 
