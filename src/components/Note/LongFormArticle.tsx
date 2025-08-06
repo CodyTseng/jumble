9f9bb2d2727a17df03fa9ee20096a7eb0f36349d
codytseng <codytseng@outlook.com>
2025-08-06 15:05:18 +0800

    feat: long-form articles

--- src/components/Note/LongFormArticle.tsx
+++ src/components/Note/LongFormArticle.tsx
@@ -1,9 +1,9 @@
 import { Badge } from '@/components/ui/badge'
 import { getLongFormArticleMetadataFromEvent } from '@/lib/event-metadata'
-import { useScreenSize } from '@/providers/ScreenSizeProvider'
 import { Event } from 'nostr-tools'
 import { useMemo } from 'react'
-import ClientSelect from '../ClientSelect'
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import Image from '../Image'
 
 export default function LongFormArticle({
@@ -13,62 +13,33 @@ export default function LongFormArticle({
   event: Event
   className?: string
 }) {
-  const { isSmallScreen } = useScreenSize()
   const metadata = useMemo(() => getLongFormArticleMetadataFromEvent(event), [event])
 
-  const titleComponent = <div className="text-xl font-semibold line-clamp-2">{metadata.title}</div>
-
-  const tagsComponent = metadata.tags.length > 0 && (
-    <div className="flex gap-1 flex-wrap">
-      {metadata.tags.map((tag) => (
-        <Badge key={tag} variant="secondary">
-          {tag}
-        </Badge>
-      ))}
-    </div>
-  )
-
-  const summaryComponent = metadata.summary && (
-    <div className="text-sm text-muted-foreground line-clamp-4">{metadata.summary}</div>
-  )
-
-  if (isSmallScreen) {
-    return (
-      <div className={className}>
-        {metadata.image && (
-          <Image
-            image={{ url: metadata.image, pubkey: event.pubkey }}
-            className="w-full aspect-video object-cover rounded-lg"
-            hideIfError
-          />
-        )}
-        <div className="space-y-1">
-          {titleComponent}
-          {summaryComponent}
-          {tagsComponent}
-          <ClientSelect className="w-full mt-2" event={event} />
-        </div>
-      </div>
-    )
-  }
-
   return (
-    <div className={className}>
-      <div className="flex gap-4">
-        {metadata.image && (
-          <Image
-            image={{ url: metadata.image, pubkey: event.pubkey }}
-            className="rounded-lg aspect-[4/3] xl:aspect-video object-cover bg-foreground h-44"
-            hideIfError
-          />
-        )}
-        <div className="flex-1 w-0 space-y-1">
-          {titleComponent}
-          {summaryComponent}
-          {tagsComponent}
+    <div className={`prose prose-zinc max-w-none dark:prose-invert ${className || ''}`}>
+      <h1>{metadata.title}</h1>
+      {metadata.summary && (
+        <blockquote>
+          <p>{metadata.summary}</p>
+        </blockquote>
+      )}
+      {metadata.tags.length > 0 && (
+        <div className="flex gap-1 flex-wrap">
+          {metadata.tags.map((tag) => (
+            <Badge key={tag} variant="secondary">
+              {tag}
+            </Badge>
+          ))}
         </div>
-      </div>
-      <ClientSelect className="w-full mt-2" event={event} />
+      )}
+      {metadata.image && (
+        <Image
+          image={{ url: metadata.image, pubkey: event.pubkey }}
+          className="w-full aspect-[3/1] object-cover rounded-lg"
+          hideIfError
+        />
+      )}
+      <Markdown remarkPlugins={[remarkGfm]}>{event.content}</Markdown>
     </div>
   )
 }
