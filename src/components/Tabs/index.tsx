da584d245bab87278ddad3584e499fdf6fd09e62
codytseng <codytseng98@gmail.com>
2025-08-23 21:32:13 +0800

    💨

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/lib/utils'
 import { useDeepBrowsing } from '@/providers/DeepBrowsingProvider'
-import { useEffect, useRef, useState } from 'react'
+import { ReactNode, useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 type TabDefinition = {
@@ -12,12 +12,14 @@ export default function Tabs({
   tabs,
   value,
   onTabChange,
-  threshold = 800
+  threshold = 800,
+  options
 }: {
   tabs: TabDefinition[]
   value: string
   onTabChange?: (tab: string) => void
   threshold?: number
+  options?: ReactNode
 }) {
   const { t } = useTranslation()
   const { deepBrowsing, lastScrollTop } = useDeepBrowsing()
@@ -30,7 +32,7 @@ export default function Tabs({
       if (activeIndex >= 0 && tabRefs.current[activeIndex]) {
         const activeTab = tabRefs.current[activeIndex]
         const { offsetWidth, offsetLeft } = activeTab
-        const padding = 32 // 16px padding on each side
+        const padding = 48 // 24px padding on each side
         setIndicatorStyle({
           width: offsetWidth - padding,
           left: offsetLeft + padding / 2
@@ -42,32 +44,35 @@ export default function Tabs({
   return (
     <div
       className={cn(
-        'sticky flex top-12 py-1 bg-background z-30 w-fit transition-transform',
+        'sticky flex justify-between top-12 py-1 bg-background z-30 w-full transition-transform',
         deepBrowsing && lastScrollTop > threshold ? '-translate-y-[calc(100%+12rem)]' : ''
       )}
     >
-      {tabs.map((tab, index) => (
+      <div className="flex w-fit">
+        {tabs.map((tab, index) => (
+          <div
+            key={tab.value}
+            ref={(el) => (tabRefs.current[index] = el)}
+            className={cn(
+              `w-fit text-center py-2 px-6 font-semibold clickable cursor-pointer rounded-lg`,
+              value === tab.value ? '' : 'text-muted-foreground'
+            )}
+            onClick={() => {
+              onTabChange?.(tab.value)
+            }}
+          >
+            {t(tab.label)}
+          </div>
+        ))}
         <div
-          key={tab.value}
-          ref={(el) => (tabRefs.current[index] = el)}
-          className={cn(
-            `w-fit text-center py-2 px-4 font-semibold clickable cursor-pointer rounded-lg`,
-            value === tab.value ? '' : 'text-muted-foreground'
-          )}
-          onClick={() => {
-            onTabChange?.(tab.value)
+          className="absolute bottom-0 h-1 bg-primary rounded-full transition-all duration-500"
+          style={{
+            width: `${indicatorStyle.width}px`,
+            left: `${indicatorStyle.left}px`
           }}
-        >
-          {t(tab.label)}
-        </div>
-      ))}
-      <div
-        className="absolute bottom-0 h-1 bg-primary rounded-full transition-all duration-500"
-        style={{
-          width: `${indicatorStyle.width}px`,
-          left: `${indicatorStyle.left}px`
-        }}
-      />
+        />
+      </div>
+      {options}
     </div>
   )
 }
